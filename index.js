d9f908a91e673f14953467257473be04b955b4f3
JosueViana <josuevianaf@gmail.com>
2023-06-30 01:13:57 -0300

    Primeiro commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const { Person } = require('./person')
+// require('./modules/path')
+// require('./modules/fs')
+// require('./modules/http')
+require('./modules/express')
+const person = new Person('Josué')
